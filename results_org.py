4d8164518faec0aa6902bd5c030593291cd3ac8c
Taosheng-ty <taoyang@cs.utah.edu>
2021-11-12 16:12:18 -0700

    update

--- results_org.py
+++ results_org.py
@@ -1,13 +1,12 @@
-
 import glob
 import os
 import pandas as pd
 import json
 import matplotlib.pyplot as plt 
 from progressbar import progressbar
-
+import numpy as np
 from pathlib import Path
-
+import pickle
 def read_json(file_path):
     with open(file_path,"r") as f:
         result=json.load(f)
@@ -32,7 +31,7 @@ def get_path_sets(root_path,same_length=False):
         pp=Path(paths[0])
         # path_root=str(pp.parent.parent)
         path_root=os.path.join(*path.split("/")[:-2])
-        # print(path_root1,path_root,"fdassa")
+#         print(path_root,path_root,"path_root")
         if same_length:
             result_cur=read_json(path)
             max_len=find_max_len(result_cur)
@@ -47,8 +46,13 @@ def merge_single_experiment_results(root_path):
     # print(paths,root_path)
     df_result_cur=pd.DataFrame()
     for path in paths:
-        pd_frame=pd.read_json(path)
+#         print(path)
+        with open(path, "r") as read_file:
+        #     print("Converting JSON encoded data into Python dictionary")
+            developer = json.load(read_file)
+            pd_frame=pd.DataFrame(developer).fillna(np.nan)    
         df_result_cur=df_result_cur.append(pd_frame)
+#     print(df_result_cur)
     return df_result_cur
 
 def merge_multiple_experiment_results(root_path):
@@ -79,23 +83,29 @@ def set_node_val(node_list,multi_level_dict,val):
         multi_level_dict=multi_level_dict[i]
     last_node=node_list[-1]
     multi_level_dict[last_node]=val
-def get_result_df(root_path,same_length=False,groupby=None):
-    path_sets=get_path_sets(root_path,same_length)
-    # print(path_sets)
-    result=AutoVivification()
-    if groupby:
+def get_result_df(root_path,same_length=False,groupby="iterations",filter_list=[],only_mean=False,rerun=False):
+    results_path=os.path.join(root_path,"results.pickle")
+    results_path_exist=glob.glob(results_path)
+#     print(results_path)
+    if results_path_exist and not rerun:
+        results_path=results_path_exist[0]
+        print("found saved results")
+        with open(results_path, 'rb') as handle:
+            result,result_mean = pickle.load(handle)
+    else:
+        path_sets=get_path_sets(root_path,same_length)
+        # print(path_sets)
+        result=AutoVivification()
         result_mean=AutoVivification()
-    for path_set in path_sets:
-        node=get_node(root_path,path_set)
-        
-        result_cur=merge_single_experiment_results(path_set)
-        set_node_val(node,result,result_cur)
-        if groupby:
-            # print(result_cur.keys(),node,result_cur)
+        for path_set in path_sets:
+            node=get_node(root_path,path_set)
+            result_cur=merge_single_experiment_results(path_set)
+            set_node_val(node,result,result_cur)
             result_merged_cur=result_cur.groupby(groupby).mean().reset_index()
             set_node_val(node,result_mean,result_merged_cur)
-        # print(result_cur,path_set)
-    if groupby:
+        with open(results_path, 'wb') as handle:
+            pickle.dump([result,result_mean], handle, protocol=pickle.HIGHEST_PROTOCOL)
+    if not only_mean:
         return result,result_mean
     else:
         return result
@@ -103,7 +113,7 @@ def get_result_df(root_path,same_length=False,groupby=None):
 import itertools
 
 def plot_metrics(name_results_pair:dict,plots_y_partition:str="metrics_NDCG",errbar=True,
-plots_x_partition:str="iterations",groupby="iterations",graph_param=None)->None:
+plots_x_partition:str="iterations",groupby="iterations",ax=None,graph_param=None)->None:
     
     '''    
         name_results_pair:{method_name:result_dataframe}
@@ -114,16 +124,23 @@ plots_x_partition:str="iterations",groupby="iterations",graph_param=None)->None:
     colors_list = prop_cycle.by_key()['color']
     colors=itertools.cycle(colors_list)
     marker = itertools.cycle((',', '+', '.', 'o', '*')) 
+    if ax:
+        plot=ax
+    else:
+        plot=plt
     for algo_name in name_results_pair:
             algo_result=name_results_pair[algo_name]
+            print(type(algo_result))
             mean=algo_result.groupby(groupby).mean().reset_index()
             std=algo_result.groupby(groupby).std().reset_index()
-            assert plots_y_partition in algo_result, algo_name+" doesn't contain the partition "+plots_y_partition
+            if plots_x_partition not in mean.keys() or plots_y_partition not in mean.keys() :
+                continue
+#             assert plots_y_partition in algo_result, algo_name+" doesn't contain the partition "+plots_y_partition
             if not errbar:
-                plt.plot(mean[plots_x_partition],mean[plots_y_partition], marker = next(marker),color=next(colors), label=algo_name)
+                plot.plot(mean[plots_x_partition],mean[plots_y_partition], marker = next(marker),color=next(colors), label=algo_name)
             else:
-                plt.errorbar(mean[plots_x_partition],mean[plots_y_partition], yerr=std[plots_y_partition], marker = next(marker),color=next(colors), label=algo_name)
-
-    gca=plt.gca()
-    gca.set(**graph_param)
-    plt.legend()
+                plot.errorbar(mean[plots_x_partition],mean[plots_y_partition], yerr=std[plots_y_partition], marker = next(marker),color=next(colors), label=algo_name)
+    if ax is None:
+        gca=plot.gca()
+        gca.set(**graph_param)
+        plot.legend()
